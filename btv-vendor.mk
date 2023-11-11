4da7bf7bc02acc7b8cd4e900183e808eb73a8819
hhyygg2009 <hhyygg2009@gmail.com>
2023-11-11 16:45:43 +0800

    test

--- btv-vendor.mk
+++ btv-vendor.mk
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_PACKAGES += \
-    HWJNIFLPLocation
-
-PRODUCT_BOOT_JARS += \
-    HWJNIFLPLocation
-
-$(call inherit-product, vendor/huawei/frd/frd-vendor-blobs.mk)
+# PRODUCT_PACKAGES += \
+#     HWJNIFLPLocation
+#
+# PRODUCT_BOOT_JARS += \
+#     HWJNIFLPLocation
+#
+# $(call inherit-product, vendor/huawei/frd/frd-vendor-blobs.mk)
